19cdf774e1fcd56b23b9b9823218103b005054b3
tvetan <tvetan@gbg.bg>
2015-01-11 10:12:12 +0200

    Init commit

---
+++ test/sample.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe("Something", function(){
+    describe("The way it smells", function(){
+        it("is a", function(){
+            assert.ok(true);
+        })
+    })
+})
